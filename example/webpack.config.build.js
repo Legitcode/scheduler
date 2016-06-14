dd7b0da2332b230848477e0edd3eff311731a7e1
dphaener <dphaener@gmail.com>
2016-06-13 22:04:55 -0700

    Add json loader to example webpack build

--- example/webpack.config.build.js
+++ example/webpack.config.build.js
@@ -15,7 +15,8 @@ module.exports = {
     loaders: [
       { test: /\.jsx$|\.es6$|\.js$/, loaders: ['react-hot', 'babel-loader?stage=0'], exclude: /node_modules/ },
       { test: /\.scss$|\.css$/, loader: 'style-loader!style!css!sass' },
-      { test: /\.(jpe?g|png|gif)$/i, loader: 'url?limit=10000!img?progressive=true' }
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'url?limit=10000!img?progressive=true' },
+      { test: /\.json$/, loader: 'json' }
     ]
   },
   plugins: [
