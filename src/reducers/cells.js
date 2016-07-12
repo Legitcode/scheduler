877dfc60c96a35ffa537bf6b7d40de064d29bdca
dphaener <dphaener@gmail.com>
2016-07-12 09:45:53 -0700

    Update eslint and fix linting errors

--- src/reducers/cells.js
+++ src/reducers/cells.js
@@ -6,18 +6,18 @@ const defaultState = Map({
 
 export default (state = defaultState, action) => {
   switch(action.type) {
-    case 'createCells':
-      const { range, resources } = action
-      let cells = Map({})
+  case 'createCells':
+    const { range, resources } = action
+    let cells = Map({})
 
-      resources.forEach(resource => {
-        range.forEach(date => {
-          cells = cells.setIn([`${resource}${date.toRef()}`], Map({ resource: resource, date: date.toRef()}))
-        })
+    resources.forEach(resource => {
+      range.forEach(date => {
+        cells = cells.setIn([`${resource}${date.toRef()}`], Map({ resource: resource, date: date.toRef()}))
       })
+    })
 
-      return fromJS({ cells: cells })
-    default:
-      return state
+    return fromJS({ cells: cells })
+  default:
+    return state
   }
 }
